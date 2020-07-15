e91a00ae4846cf9e0a1420cb13bbc64a94bba9e8
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2020-07-15 17:09:34 +0900

    user registration logic

--- config/db.js
+++ config/db.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 // mongoose.connect(db)
 
 const connectDB = async () => {
-    try {
-       await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-       console.log("MongoDB Connected...");
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-}
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
 
 // router.get("/", (req, res) => res.send("get request test"));
 router.post(
@@ -13,13 +16,53 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User post");
+
+    const { name, email, password } = req.body;
+
+    try {
+      //user exists	email: email == email
+      let user = await User.findOne({ email });
+      if (user) {
+        //consistency
+        //using return prevents multiple responses
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      //get gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      //not saved yet!
+      user = new User({
+        name,
+        email,
+        avatar,
+        password, //not encrypted yet
+      });
+
+      //encrypt passowrd
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //return jwt
+
+      res.send("User registered successfully");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
