0596228813bd3849d413dbbf229926f69aa3654b
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2020-07-16 17:02:38 +0900

    auth middleware & jwt verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require('../../models/User');
 
-router.get("/", (req, res) => res.send("Auth route"));
+//pass middleware just like that
+router.get("/", auth, async (req, res) => {
+//   res.send("Auth route");
+try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+} catch (error) {
+    console.error(err.message);
+    return res.status(500).send('Server Error')
+}
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
@@ -56,9 +58,25 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      //return jwt
 
-      res.send("User registered successfully");
+      //return jwt
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //res.send("User registered successfully");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
