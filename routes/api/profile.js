37900d86e46fa13b0524deba89ff2d91563b952f
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2020-07-29 10:51:31 +0900

    delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -215,4 +215,29 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/experience
+// @desc    Delete experience from profile
+// @acess   Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    // console.log(removeIndex);
+
+    if (removeIndex === -1)
+      return res.status(400).json({ msg: "Experience not found" });
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
